f808fa3aea34fe748db792992d9802aea85d706b
Anthony Cieri <penguinmillion@gmail.com>
2024-10-24 13:50:31 -0700

    feat: Slightly less basic page builder

--- pageBuilder/pageBuilder.py
+++ pageBuilder/pageBuilder.py
@@ -1,10 +1,9 @@
 
-import sys
 import os
 
 
 
-assembled_pages_dir = "./assembledPages/"
+assembled_pages_dir = "./"
 
 page_parts_dir = "./pageParts/"
 
@@ -13,15 +12,46 @@ snippets_dir = "./pageParts/snippets/"
 insert_code = [ "<!-- ##",  "## -->" ]
 
 
-snippet_code_html: dict = dict();
+page_html: dict = dict()
+snippet_code_html: dict = dict()
 
 
 
 def assemble_pages():
 
+    get_pages()
+
+    make_action_snippets()
+
     get_snippets()
 
-    insert_snippets()
+    insert_snippets_into_pages()
+
+
+
+def get_pages():
+    global page_html
+
+    for file in filter(lambda file: file.endswith('.html'), os.listdir(page_parts_dir)):
+        with open(f"{page_parts_dir}{file}", 'r') as f:
+            page_html[file] = f.read()
+
+    
+
+
+
+def make_action_snippets():
+    global snippet_code_html
+    
+    navigation_snippet = "<div class=\"navigation\">\n"
+
+    for page in page_html.keys():
+        g = ""
+        navigation_snippet += f"<a href=\"./{page}\">{page.removesuffix(".html").title()}</a>\n"
+
+    navigation_snippet += "</div>\n"
+
+    snippet_code_html[snippet_name_to_code("navigation")] = navigation_snippet
 
 
 
@@ -30,26 +60,35 @@ def get_snippets():
 
     for file in filter(lambda file: file.endswith('.html'), os.listdir(snippets_dir)):
         with open(f"{snippets_dir}{file}", 'r') as f:
-            snippet_code_html[f"{insert_code[0]}{file[:-5]}{insert_code[1]}"] = f.read()
+            snippet_code_html[snippet_name_to_code(file.removesuffix(".html"))] = insert_snippets(f.read())
+
+
+
+def snippet_name_to_code(snippet: str) -> str:
+    return f"{insert_code[0]}{snippet}{insert_code[1]}"
 
 
 
 
-def insert_snippets():
+def insert_snippets_into_pages():
 
-    for file in filter(lambda file: file.endswith('.html'), os.listdir(page_parts_dir)):
-        assembled_page = ""
+    for page in page_html.keys():
+        with open(f"{assembled_pages_dir}{page[:-5]}.html", 'w') as f:
+            f.write(insert_snippets(page_html[page]))
 
-        with open(f"{page_parts_dir}{file}", 'r') as f:
 
-            for line in f.readlines():
-                assembled_page += line
-                for snippet_code in snippet_code_html.keys():
-                    if line.find(snippet_code) >= 0:
-                        assembled_page += snippet_code_html[snippet_code]
+
+
+def insert_snippets(template: str) -> str:
         
-        with open(f"{assembled_pages_dir}{file[:-5]}.html", 'w') as f:
-            f.write(assembled_page)
+    assembled_page = template
+
+    for snippet_code in snippet_code_html.keys():
+        assembled_page = assembled_page.replace(snippet_code, snippet_code_html[snippet_code])
+
+    return assembled_page
+
+
 
 
 
